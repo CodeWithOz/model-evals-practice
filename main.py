08da7b67de1d3b0387f5a180f097eeb4c1cb2991
Uche Ozoemena <codewithoz@outlook.com>
2025-08-31 02:49:45 -0400

    feat: create data lookup tool

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from dotenv import load_dotenv
+import logging
+from tools.lookup_sales import lookup_sales_data
+
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+load_dotenv()
+
 def main():
-    print("Hello from model-evals-practice!")
+    lookup_sales_data("Show me all the sales for store 1320 on November 1st, 2021")
 
 
 if __name__ == "__main__":
---
+++ tools/lookup_sales.py
@@ -0,0 +1,56 @@
+from openai import OpenAI
+import pandas as pd
+import duckdb
+import logging
+
+logger = logging.getLogger(__name__)
+
+MODEL = "gpt-4o-mini"
+TRANSACTION_DATA_FILE_PATH = "data/Store_Sales_Price_Elasticity_Promotions_Data.parquet"
+
+SQL_GENERATION_PROMPT = """
+Generate an SQL query based on a prompt. Do not reply with anything besides the SQL query.
+The prompt is: {prompt}
+
+The available columns are: {columns}
+The table name is: {table_name}
+"""
+
+
+def generate_sql_query(prompt: str, columns: list, table_name: str) -> str:
+    """Generate an SQL query based on a prompt"""
+    formatted_prompt = SQL_GENERATION_PROMPT.format(
+        prompt=prompt, columns=columns, table_name=table_name
+    )
+
+    logger.info("Requesting sql query from model")
+    response = OpenAI().chat.completions.create(
+        model=MODEL, messages=[{"role": "user", "content": formatted_prompt}]
+    )
+    logger.info("Received sql query from model")
+
+    return response.choices[0].message.content
+
+
+def lookup_sales_data(prompt: str) -> str:
+    table_name = "sales"
+
+    # initialize SQL table
+    logger.info("Creating db from parquet file")
+    df = pd.read_parquet(TRANSACTION_DATA_FILE_PATH)
+    duckdb.sql(f"CREATE TABLE IF NOT EXISTS {table_name} AS SELECT * FROM df")
+    logger.info("Created db from parquet file")
+
+    # get the SQL query to execute
+    sql_query = generate_sql_query(prompt, df.columns.tolist(), table_name)
+
+    # remove ``` from the query
+    sql_query = sql_query.replace("```sql", "").replace("```", "").strip()
+
+    # execute the chatbot's query
+    logger.info("Executing query from model")
+    result = duckdb.sql(sql_query)
+    result.show()
+    logger.info("Executed query from model")
+
+    return result.df().to_string()
