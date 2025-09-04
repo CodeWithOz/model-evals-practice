2029276b006f1fcac0a780b2c5cf804fc9d15dd1
Uche Ozoemena <codewithoz@outlook.com>
2025-09-04 18:14:28 +0100

    feat: support inference via cerebras

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from openai import OpenAI
+from cerebras.cloud.sdk import Cerebras
 from pydantic import BaseModel
+import os
 
 
 def get_model():
@@ -10,10 +12,12 @@ def get_model():
         "api_key": "lm-studio",
     }
     return OpenAI(**kwargs)
+    # return Cerebras(api_key=os.environ.get("CEREBRAS_API_KEY"))
 
 
 def invoke_model(
-    model: OpenAI,
+    # model: OpenAI,
+    model: Cerebras,
     prompt: Optional[str] = None,
     messages: Optional[list] = None,
     tools: Optional[list] = None,
@@ -31,12 +35,14 @@ def invoke_model(
         messages=messages,
         # model="gpt-4o-mini",
         model="google/gemma-3n-e4b",
+        # model="gpt-oss-120b",
         tools=tools,
     )
 
 
 def invoke_model_with_structured_output(
-    model: OpenAI,
+    # model: OpenAI,
+    model: Cerebras,
     output_format: BaseModel,
     prompt: Optional[str] = None,
     messages: Optional[list] = None,
@@ -55,6 +61,7 @@ def invoke_model_with_structured_output(
         messages=messages,
         # model="gpt-4o-mini",
         model="google/gemma-3n-e4b",
+        # model="gpt-oss-120b",
         tools=tools,
         response_format=output_format,
     )
